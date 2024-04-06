45964f33cf5f6241dfc6216402619c5bcf8e87bb
Gulrez Alam <egulrezalam@gmail.com>
2024-04-07 03:59:11 +0530

    Updated views.py and added a new file

--- accounts/views.py
+++ accounts/views.py
@@ -3,20 +3,6 @@ from django.contrib.auth import get_user_model
 from .forms import SignupForm, GetStartedForm
 from .models import UserRegistration
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_str
-from django.contrib.auth.tokens import account_activation_token
-from django.contrib.auth import login
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import send_mail
-from django.conf import settings
-
-
-
-
 
 User = get_user_model()
 
@@ -48,31 +34,12 @@ def get_started(request):
     
     if request.method == 'POST':
         form = GetStartedForm(request.POST, request.FILES, instance=user_registration)
+        print('Form checking...')
         print(form.errors)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'],
-                                                email=form.cleaned_data['email'],
-                                                password=form.cleaned_data['password'],
-                                                profile_picture=form.cleaned_data['profile_picture'],
-                                                location=form.cleaned_data['location'])
             form.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your account'
-            message = render_to_string('account_activation_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            send_mail(
-                mail_subject,
-                message,
-                settings.EMAIL_HOST_USER,
-                [to_email],
-            )
-            return HttpResponse('Please confirm your email address to complete the registration')
-
+            print('Form saved')
+            return redirect('home')  # Redirect to the home page after form submission
     else:
         form = GetStartedForm(instance=user_registration)
     
@@ -81,18 +48,4 @@ def get_started(request):
 
 
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        return HttpResponse('Your account has been activated successfully')
-    else:
-        return HttpResponse('Activation link is invalid or expired')
 
